3a40160dcb17bd8d7d8496f2059e66d6ac98ef05
Anupam <anupambhatt@gmail.com>
2013-03-28 13:28:28 +0530

    Checkin the initial code

---
+++ src/org/mrbhatt/duplicatefinder/exceptions/InvalidPathException.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.mrbhatt.duplicatefinder.exceptions;
+
+/**
+ * @author ABy
+ *
+ */
+public class InvalidPathException extends Exception 
+{
+	public InvalidPathException() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+}
